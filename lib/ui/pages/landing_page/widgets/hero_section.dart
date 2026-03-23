c1bd67348c4c5c3e0d90aeb94bb4de962abe1ab2
Alfredo <alfredobautista1@gmail.com>
2026-03-23 22:18:45 +0100

    feat: Integrate authentication login into the hero section

--- lib/ui/pages/landing_page/widgets/hero_section.dart
+++ lib/ui/pages/landing_page/widgets/hero_section.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pizzathon/ui/blocs/auth_cubit.dart';
 
 class HeroSection extends StatelessWidget {
   const HeroSection({super.key});
@@ -62,29 +64,17 @@ class HeroSection extends StatelessWidget {
             width: 240,
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).colorScheme.primary.withOpacity(0.8),
+                backgroundColor: Theme.of(context).colorScheme.primary,
                 foregroundColor: Colors.white,
                 padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
                 elevation: 0,
                 shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
               ),
               onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: const Text(
-                      '¡Abriremos las inscripciones muy pronto!',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-                    ),
-                    backgroundColor: Theme.of(context).colorScheme.secondary,
-                    behavior: SnackBarBehavior.floating,
-                    width: isMobile ? null : 400,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                  ),
-                );
+                context.read<AuthCubit>().login();
               },
               child: const Text(
-                'Próximamente',
+                'Me interesa',
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
             ),
