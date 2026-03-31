3a7d727c90076b3aeabe4a45aa68e78b7f363a8a
Alfredo <alfredobautista1@gmail.com>
2026-03-31 10:54:06 +0200

    feat: update default enrollment status to false

--- lib/data/services/remote_config_service.dart
+++ lib/data/services/remote_config_service.dart
@@ -6,25 +6,26 @@ class RemoteConfigService {
 
   Future<void> init() async {
     try {
-      await _remoteConfig.setDefaults(const {
-        "is_enrollment_open": true, 
-      });
+      await _remoteConfig.setDefaults(const {"is_enrollment_open": false});
 
-      await _remoteConfig.setConfigSettings(RemoteConfigSettings(
-        fetchTimeout: const Duration(minutes: 1),
+      await _remoteConfig.setConfigSettings(
+        RemoteConfigSettings(
+          fetchTimeout: const Duration(minutes: 1),
 
-        minimumFetchInterval: kDebugMode ? const Duration(minutes: 1) : const Duration(minutes: 5),
-      ));
+          minimumFetchInterval: kDebugMode
+              ? const Duration(minutes: 1)
+              : const Duration(minutes: 5),
+        ),
+      );
 
       await _remoteConfig.fetchAndActivate();
-      
     } catch (e) {
       debugPrint("Error inicializando Remote Config: $e");
     }
   }
 
   bool get isEnrollmentOpen => _remoteConfig.getBool("is_enrollment_open");
-  
+
   Future<void> forceFetch() async {
     await _remoteConfig.fetchAndActivate();
   }
