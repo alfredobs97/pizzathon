888ea2c15f715d5c8d6e245460a53b77fe3a653b
Alfredo Bautista <alfredobautista1@gmail.com>
2026-04-06 20:00:13 +0200

    feat: implement admin role authorization and add restricted admin route (#32)

---
+++ lib/domain/models/user_extension.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+extension UserExtension on User {
+  bool get isAdmin {
+    final adminEmails = [
+      'baldizzone42@gmail.com',
+      'alfredobautista1@gmail.com',
+      'salvajeguerrero@gmail.com',
+      'salvapizzalover@gmail.com',
+    ];
+    return adminEmails.contains(email);
+  }
+}
