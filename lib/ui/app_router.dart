89a26c17bb63b955f9fc5e94bafb523ead672623
Alfredo <alfredobautista1@gmail.com>
2026-04-06 19:51:34 +0200

    feat: implement admin role authorization and add restricted admin route

--- lib/ui/app_router.dart
+++ lib/ui/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:pizzathon/domain/models/user_extension.dart';
 import 'package:pizzathon/ui/blocs/auth_cubit.dart';
 import 'package:pizzathon/ui/blocs/auth_state.dart';
 import 'package:pizzathon/ui/pages/home/home_page.dart';
@@ -9,6 +10,7 @@ import 'package:pizzathon/ui/pages/landing_page/landing_page.dart';
 class AppRouter {
   static const String landingRoute = '/';
   static const String participantsRoute = '/participantes';
+  static const String adminRoute = '/capo';
 
   final _router = GoRouter(
     initialLocation: landingRoute,
@@ -23,11 +25,23 @@ class AppRouter {
           return const Scaffold(body: Center(child: CircularProgressIndicator()));
         },
       ),
+      GoRoute(
+        path: adminRoute,
+        builder: (context, state) => Scaffold(
+          body: Center(
+            child: Image.network(
+              'https://www.shutterstock.com/shutterstock/photos/1470841721/display_1500/stock-photo-the-circle-game-meme-fingers-making-the-ok-symbol-meme-internet-popular-prank-game-if-you-look-1470841721.jpg',
+            ),
+          ),
+        ),
+      ),
     ],
     redirect: (context, state) {
-      final authState = context.read<AuthCubit>().state;
+      if (state.matchedLocation == adminRoute && !isAdmin(context)) {
+        return landingRoute;
+      }
 
-      if (state.matchedLocation == participantsRoute && authState is! AuthAuthenticated) {
+      if (state.matchedLocation == participantsRoute && !isAuth(context)) {
         return landingRoute;
       }
 
@@ -36,4 +50,14 @@ class AppRouter {
   );
 
   GoRouter get router => _router;
+
+  static bool isAuth(BuildContext context) {
+    final authState = context.read<AuthCubit>().state;
+    return authState is AuthAuthenticated;
+  }
+
+  static bool isAdmin(BuildContext context) {
+    final authState = context.read<AuthCubit>().state;
+    return authState is AuthAuthenticated && authState.user.isAdmin;
+  }
 }
